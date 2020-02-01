132f25348900f2682085c237f4b2dd66faf84fae
Mohammed Ali <mali@teladoc.com>
2020-02-01 16:33:24 -0600

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,29 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import Markdown from 'react-markdown';
+import event  from './events/jan-31-2020.md';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props){
+    super();
+    this.fetchEvent = this.fetchEvent.bind(this);
+  }
+
+  async fetchEvent(file) {
+    try {
+      const resp = await fetch(file)
+      console.log(resp.body)
+      return resp.text();
+    } catch (err) {
+      console.log(err)
+    }
+  }
+
+  render() {
+    return (
+      <Markdown source={this.fetchEvent(event)} />
+    )
+  }
 }
 
 export default App;
